a71deb889c119c88d494c29ba9b15c465263dd21
Denis Golovin <denis_golovin@mail.ua>
2017-04-06 00:35:32 +0300

    almost

---
+++ task_02/task/src/GithubUsers.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import UserComand from './UserComand'
+
+class GithubUsers extends Component {
+	state = {
+		users: [],
+		nextUsers: [],
+		nextTen: 10,
+		isLoading: false
+	}
+	componentDidMount() {
+		fetch('https://api.github.com/users')
+				.then(users => users.json())
+				.then(json => {
+					this.setState({users: json});
+					this.setState({nextUsers: this.state.users.slice(0,this.state.nextTen)})
+				})
+	}
+	addNewUsers = (e) => {
+		let {users} = this.state,
+				{nextUsers} = this.state,
+				{nextTen} = this.state
+
+		if(users.length === nextUsers.length) {
+
+			this.setState({isLoading: true});
+
+			var newUsersUrl = 'https://api.github.com/users?since=';
+			fetch(newUsersUrl + users.length)
+			.then(users => users.json())
+			.then(json => {
+					this.setState({users : users.concat(json)})
+				})
+			.then(() => {
+				this.setState({nextTen: (nextTen + 10)})
+				this.setState({nextUsers: this.state.users.slice(0, (this.state.nextTen))});
+			})
+			.then(() => {
+				this.setState({isLoading: false})
+			})
+		} else {
+			if (users.length - nextUsers.length === 10) {
+				e.target.textContent = 'Load More';
+			}
+			this.setState({nextTen: (nextTen + 10)});
+			this.setState({nextUsers: users.slice(0, (nextTen + 10))});
+		}
+	}
+	render() {
+		var loading;
+		if (this.state.isLoading) {
+			loading = 'Loading...'
+		}
+		return (
+			<div style={{textAlign: 'center'}}>
+				<UserComand users={this.state.nextUsers} />
+				<button onClick={this.addNewUsers} style={{width: '50%'}}>{loading || 'Show More'}</button>
+			</div>
+		);
+	}
+}
+
+export default GithubUsers;
---
+++ task_02/task/src/UserComand.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class UserComand extends Component {
+	state = {
+		userName: ''
+	}
+	addName = (e) => {
+		var nameUrl = 'https://api.github.com/user/'
+    fetch(nameUrl + e.target.getAttribute(name))
+    	.then(data => data.json())
+    	.then(json => {
+    		this.setState({userName: json.name})
+    	})
+   }
+	render() {
+		return (
+			<div className="Content">
+				<h1>Here our Figures</h1>
+				<ul>
+				{this.props.users.map((user, i) => {
+					return (
+						<li key={i}>
+							<figure>
+							<img name={i} onClick={this.addName} src={user.avatar_url} alt='avatar'/>
+							<a target="_blank" href={user.html_url}>{user.login}</a>
+							</figure>
+						</li>
+						);
+				})}
+				</ul>
+			</div>
+			);
+	}
+}
+
+export default UserComand;
