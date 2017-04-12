c5ab35947d466faba2ec0b47a050c5e2d1d3c74f
Denis Golovin <denis_golovin@mail.ua>
2017-04-13 00:58:32 +0300

    almost

--- task_02/task/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GithubUsers from './GithubUsers';
-
-ReactDOM.render(
-  <GithubUsers />,
-  document.getElementById('root')
-);
